81cfb581a997a52cc30e20e6f1220e8cea26c3b6
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-03-28 00:39:23 +0530

    Initial commit

---
+++ utils/auth.py
@@ -0,0 +1,14 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+uri=os.getenv("URI")
+
+client = MongoClient(uri, server_api=ServerApi('1'))
+try:
+    client.admin.command('ping')
+    print("Connected to MongoDB!")
+except Exception as e:
+    print(e)
