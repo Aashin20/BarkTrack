fb860c61c1de3bbe573e9472df5a20d223e219f2
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-03-29 13:28:23 +0530

    Update auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -3,23 +3,15 @@ from pymongo.server_api import ServerApi
 import os
 from dotenv import load_dotenv
 import bcrypt
+from .db import Database
 
 load_dotenv()
 
 def register(name,email,password):
-    uri=os.getenv("URI")
-    client = MongoClient(uri, server_api=ServerApi('1'))
-    try:
-        client.admin.command('ping')
-        print("Connected to MongoDB!")
-    except Exception as e:
-        print(e)
-    db=client.auth_db
-    users=db.users
-
+    users = Database.get_db().users
     exists = users.find_one({"email":email})
     if exists:
-        return "User already exists"
+        return {"message":"User already exists"}
     
     else:
         bytes=password.encode('utf-8')
@@ -33,7 +25,7 @@ def register(name,email,password):
         }
         
         users.insert_one(info)  
-        return "Registered Successfully"
+        return {"message":"Registered Successfully"}
     
 
     
