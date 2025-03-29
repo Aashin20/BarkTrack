a4e8297a72823b4a9a87b1f68410d938e8f42edd
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-03-29 11:33:07 +0530

    Implemented password hashing

--- utils/auth.py
+++ utils/auth.py
@@ -2,13 +2,24 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import os
 from dotenv import load_dotenv
+import bcrypt
 
 load_dotenv()
 
-uri=os.getenv("URI")
-client = MongoClient(uri, server_api=ServerApi('1'))
-try:
-    client.admin.command('ping')
-    print("Connected to MongoDB!")
-except Exception as e:
-    print(e)
+def register(name,email,password):
+    uri=os.getenv("URI")
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    try:
+        client.admin.command('ping')
+        print("Connected to MongoDB!")
+    except Exception as e:
+        print(e)
+
+    bytes=password.encode('utf-8')
+    salt=bcrypt.gensalt()
+    hash=bcrypt.hashpw(bytes,salt)
+
+    
+ 
+
+
