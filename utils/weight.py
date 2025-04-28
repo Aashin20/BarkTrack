af06721272834233d9b247d60554b559674b1993
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-04-28 11:12:42 +0530

    Weigth identification v1

---
+++ utils/weight.py
@@ -0,0 +1,32 @@
+from groq import Groq
+import base64
+import os
+import ast
+
+def weight_identification(img: str) :
+    client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "Estimate the weight(in kg) and body composition score(out of 10) of the dog in the image. ONLY OUTPUT THE APPROXIMATE WEIGHT AND BODY COMPOSITION SCORE AND NOTHING ELSE.DO NOT RETURN RANGE VALUES. THE OUTPUT SHOULD BE IN THE FOLLOWING FORMAT: {'weight': <weight>, 'body_composition_score': <score>}."},
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{img}",
+                        },
+                    },
+                ],
+            }
+        ],
+        model="meta-llama/llama-4-scout-17b-16e-instruct",
+    )
+
+    content= chat_completion.choices[0].message.content
+    return ast.literal_eval(content)
+
+
+
+
