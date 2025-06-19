1811558a1f144da059935bded56e927c2a480563
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-20 00:55:11 +0530

    Create fn to predict frame

---
+++ utils/profile.py
@@ -0,0 +1,46 @@
+from bson import ObjectId
+from .db import Database
+from fastapi import HTTPException
+
+def add_dog_to_user(user_id: str, dog_data: dict):
+    users = Database.get_db().users
+
+    result = users.update_one(
+        {"_id": ObjectId(user_id)},
+        {"$push": {"dogs": dog_data}}
+    )
+
+    if result.modified_count == 1:
+        return {"message": "Dog added successfully"}
+    else:
+        raise HTTPException(status_code=404, detail="User not found or dog not added")
+
+def get_user_dogs(user_id: str):
+    users = Database.get_db().users
+    user = users.find_one({"_id": ObjectId(user_id)}, {"dogs": 1})
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return user.get("dogs", [])
+
+def get_user_details(user_id: str):
+    users = Database.get_db().users
+    user = users.find_one(
+        {"_id": ObjectId(user_id)},
+        {"password": 0}  
+    )
+    
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    user["_id"] = str(user["_id"])
+    return user
+
+def doctor_helper(doctor) -> dict:
+    return {
+        "id": str(doctor["_id"]),
+        "name": doctor["name"],
+        "phone_number": doctor["phone_number"],
+        "specialty": doctor["specialty"],
+        "address": doctor["address"],
+    }
